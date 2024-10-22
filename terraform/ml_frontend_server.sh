dc2634db0264dc29c2c21815d6168eed90a7e134
luis <morenodoesinfra@gmail.com>
2024-10-22 19:10:23 +0000

    requirements for web app

--- terraform/ml_frontend_server.sh
+++ terraform/ml_frontend_server.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install redis-server
+
 sudo apt install python3-pip python3-venv
+
 
 # download repo
 git clone https://github.com/elmorenox/CNN_deploy.git
 
-cd ~/CNN_DEPLOY/pneumonia_api
+cd ~/CNN_DEPLOY/pneumonia_web
+
 python3 -m venv venv
 
 source venv/bin/active
