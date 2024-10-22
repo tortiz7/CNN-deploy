360b28d87e62d3cee9618547abd6f8b0b04b9ea7
luis <morenodoesinfra@gmail.com>
2024-10-22 23:28:59 +0000

    more htmx centrix frontend

--- pneumonia_web/app.py
+++ pneumonia_web/app.py
@@ -9,24 +9,27 @@ ITEMS_PER_PAGE = 50
 
 @app.route('/')
 def index():
-    # Get the current page number from query parameters, default to 1
+    return render_template('index.html')
+
+@app.route('/table')
+def table():
     page = request.args.get('page', 1, type=int)
-    
-    # Get all keys from Redis
     keys = r.keys('*')
     total_keys = len(keys)
-
-    # Calculate pagination
+    
     start = (page - 1) * ITEMS_PER_PAGE
     end = start + ITEMS_PER_PAGE
     keys_to_display = keys[start:end]
 
-    # Get the data for the keys to display
     data = {}
     for key in keys_to_display:
         data[key] = r.hgetall(key)
 
-    return render_template('index.html', data=data, page=page, total_keys=total_keys, items_per_page=ITEMS_PER_PAGE)
+    return render_template('table_partial.html', 
+                         data=data, 
+                         page=page, 
+                         total_keys=total_keys, 
+                         items_per_page=ITEMS_PER_PAGE)
 
 if __name__ == '__main__':
     app.run(debug=True)
