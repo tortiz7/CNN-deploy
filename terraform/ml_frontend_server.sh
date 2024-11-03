7e361c73498f29e19fc9be06747c01a41195d1f5
Triston Ortiz <taokuralabs@gmail.com>
2024-11-03 15:02:57 -0500

    Update ml_frontend_server.sh

--- terraform/ml_frontend_server.sh
+++ terraform/ml_frontend_server.sh
@@ -12,7 +12,7 @@ apt-get install -y python3-venv
 
 # download repo
 cd /home/ubuntu
-git clone https://github.com/kura-labs-org/AIWL1.git /home/ubuntu/CNN_deploy
+git clone https://github.com/tortiz7/Pnu-AI.git /home/ubuntu/CNN_deploy
 # Set permissions on the repo
 sudo chown -R ubuntu:ubuntu /home/ubuntu/CNN_deploy
 cd CNN_deploy/pneumonia_web
